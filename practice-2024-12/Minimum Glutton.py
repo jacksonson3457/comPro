a7313c1ce90b494a99cf80bf3fd6801e90f169ca
yuto33547 <monogatari9010@gmail.com>
2024-12-13 16:35:39 +0900

    最初のコードはめちゃ惜しくてほとんど正解だった

--- practice-2024-12/Minimum Glutton.py
+++ practice-2024-12/Minimum Glutton.py
@@ -1,30 +1,17 @@
-from itertools import permutations
+def canEat(maxValue, array):
+    counter = 0
+    current = 0
+    for v in array:
+        
+        current += v
+        counter += 1
 
-def minimumDishes(N, X, Y, sweaty, salty):
-    #甘さとしょっぱさをひとまとまりにする
-    dishes = list(zip(sweaty, salty))
-    minCount = N
-
-    for order in permutations(dishes):
-        sweetness, saltyness = 0, 0
-        count = 0
-        for a, b in order:
-            sweetness += a
-            saltyness += b
-            count += 1
-
-            if sweetness > X or saltyness > Y:
-                break
-        minCount = min(minCount, count)
-    
-    return minCount
-
-
-    
-    
-
+        # 食べた後に制約を確認する
+        if current > maxValue:
+            break
+    return counter
 
 N, X, Y = map(int, input().split())
 sweaty = sorted(map(int, input().split()), reverse=True)
 salty = sorted(map(int, input().split()), reverse=True)
-print(minimumDishes(N, X, Y, sweaty, salty))
+print(min(canEat(X, sweaty), canEat(Y, salty)))
